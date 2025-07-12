c57c245f78890e968ae6454c9b5b4f4709a9c195
mizuki <sma0729mex@gmail.com>
2025-07-12 16:22:16 +0900

    feat: add tailwind (#3)
    * fix: add standalone component configuration for Angular app
    
    * chore: add `tailwind`

--- apps/frontend/src/app/app.ts
+++ apps/frontend/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `
+    <h1 class="text-5xl font-bold underline">{{ title }}</h1>
+  `
 })
 export class App {
   protected title = 'frontend';
